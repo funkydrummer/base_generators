cdd8a9107bb18d7dc0be89bb53cedd7a141e5179
Rafał Trojanowski <rafal@prograils.com>
2014-01-24 22:13:58 +0100

    indent under private

--- lib/generators/base_generators/admin/templates/controller/controller.rb
+++ lib/generators/base_generators/admin/templates/controller/controller.rb
@@ -1,21 +1,21 @@
 class Admin::<%= class_name %>Controller < Admin::BaseController
   inherit_resources
-  
+
   <%- if options.private_methods? -%>
   private
 
-  def collection
-    return get_collection_ivar if get_collection_ivar
+    def collection
+      return get_collection_ivar if get_collection_ivar
 
-    @q = end_of_association_chain.search(params[:q])
-    @q.sorts = 'created_at desc' if @q.sorts.blank?
-    result = @q.result.page(params[:page]).per(params[:per_page])
+      @q = end_of_association_chain.search(params[:q])
+      @q.sorts = 'created_at desc' if @q.sorts.blank?
+      result = @q.result.page(params[:page]).per(params[:per_page])
 
-    set_collection_ivar(result)
-  end
-  
-  def permitted_params
-    params.permit(<%= resource %>: <%= attributes %>)
-  end
+      set_collection_ivar(result)
+    end
+
+    def permitted_params
+      params.permit(<%= resource %>: <%= attributes %>)
+    end
   <%- end -%>
 end
